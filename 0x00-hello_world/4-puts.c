3f54c6f27cf841f56ef477c326fb9a20fbff21ff
Your Name <eliber.za.221@gmail.com>
2022-02-03 20:48:09 -0800

    cftas4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "\"Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (two success)
+ */
+
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
