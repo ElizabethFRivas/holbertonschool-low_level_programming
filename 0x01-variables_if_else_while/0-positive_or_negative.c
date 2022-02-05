4a4a941a1bac7718c17503e71f87834759804988
Your Name <eliber.za.221@gmail.com>
2022-02-04 16:51:40 -0800

    correccion del task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,18 +9,19 @@
  * Return: Always 0 (success)
  */
 
+
 int main(void)
 {
-	int n; 
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positive",n);
+	printf("%d is positive", n);
 	if (n == 0)
-		printf("%d is zero",n);
+	printf("%d is zero ", n);
 	if (n < 0)
-		printf("%d is negative",n);
-	
-	return (0); 
+	printf("%d is negative", n);
+	return (0);
 }
