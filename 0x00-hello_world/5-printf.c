de1dc324a1baaeacc9369fe5a9db4b9111dce823
Your Name <eliber.za.221@gmail.com>
2022-02-07 07:50:57 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "\"Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (two success)
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art\n");
+        return (0);
+}
