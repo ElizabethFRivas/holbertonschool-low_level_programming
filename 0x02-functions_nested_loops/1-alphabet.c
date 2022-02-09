71db5711bd46a9e08297f19a199fee71ac95e339
Your Name <eliber.za.221@gmail.com>
2022-02-09 11:16:39 -0800

    correccion task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always void.
+ */
+
+void print_alphabet(void)
+{
+	int  inicio = 97;
+
+	while (inicio < 123)
+	{
+		_putchar(inicio);
+		_putchar('\n');
+		inicio = inicio + 1;
+
+	}
+}
