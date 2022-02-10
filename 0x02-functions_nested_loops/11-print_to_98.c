a43abe9bd1bbc03d433d1be90df80712d7cb14ab
Your Name <eliber.za.221@gmail.com>
2022-02-09 19:05:15 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - Write a function that prints the last digit of a number
+ * @n: integer
+ * Return: Always void.
+ */
+
+void print_to_98(int n)
+{
+	int a = 0;
+
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if ( a == 1)
+				printf(", ");
+			printf("%d", n);
+			a = 1;
+
+		}
+		putchar('\n');
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			if ( a == 1)
+				printf(", ");
+			printf("%d", n);
+			a = 1;
+
+		
+		}
+		putchar('\n');
+
+	}
+	else 
+		printf("%d\n", n);
+}			
